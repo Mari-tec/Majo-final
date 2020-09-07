efce399b0aec46395b8db903f6e394ac922375b6
Mari-tec <70905102+Mari-tec@users.noreply.github.com>
2020-09-07 18:22:05 -0500

    Add files via upload

---
+++ indexj.js
@@ -0,0 +1,6 @@
+document.getElementById("a").innerHTML = "Bienvenido al juego";
+document.getElementById("s").innerHTML = "En este juego habran 8 preguntas ";
+document.getElementById("d").innerHTML = "debes escoger entre a b c, la mas";
+document.getElementById("f").innerHTML = "factible, luego te mostrara si te ";
+document.getElementById("g").innerHTML = "equivocaste o si acertaste.";
+document.getElementById("h").innerHTML = "Estas listo?";
